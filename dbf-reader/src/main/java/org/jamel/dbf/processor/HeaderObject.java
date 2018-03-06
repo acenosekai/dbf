b9e43ce8be18aa719cbaae01bdff685a1a5c8a4b
septiyan <acenosekai@gmail.com>
2018-03-06 11:19:23 +0700

    ad header object

---
+++ dbf-reader/src/main/java/org/jamel/dbf/processor/HeaderObject.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jamel.dbf.processor;
+
+/**
+ *
+ * @author Datacom
+ */
+public class HeaderObject {
+    private Integer index;
+    private String name;
+    private String type;
+    private String length;
+    private String decimal;
+}
